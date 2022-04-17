4474fcd56de05b453d438cc13bb629e8549c8134
Karthik R <iamjustkarthik@gmail.com>
2022-04-17 23:47:06 +0530

    Modified Form

--- script.js
+++ script.js
@@ -18,10 +18,7 @@ nextBtns.forEach((btn) => {
   });
 });
 
-// console.log(mainancor)
-
 prevBtns.forEach((btn) => {
-  // console.log(formStepsNum)
   btn.addEventListener("click", () => {
     formStepsNum--;
     updateFormSteps();
@@ -45,7 +42,6 @@ function updateFormSteps() {
 
   })
   console.log(formStepsNum)
-  // console.log(stepbardiv[formStepsNum-1].classList)
   formSteps[formStepsNum].classList.add("form-step-active");
   let inancor=formSteps[formStepsNum].querySelectorAll('a');
   console.log(mainancor[formStepsNum].classList)
@@ -61,86 +57,79 @@ function jumpForm(stepNumber)
 {
   console.log(stepNumber)
   formStepsNum=stepNumber-1
-  // formSteps.forEach((formStep) => {
-  //   formStep.classList.contains("form-step-active") &&
-  //     formStep.classList.remove("form-step-active");
-  // });
-
-  // formSteps[formStepsNum].classList.add("form-step-active");
   updateFormSteps()
-
 }
-function validateEmail(){
-  let emailid=document.getElementById("email")
-  let email=document.getElementById("email").value;
-  let errormessage=document.getElementById("emailError");
-  let emailRequired=document.getElementById("emailRequired");
-  let testREgex='[a-z0-9]+@[a-z]+\.[a-z]{2,3}';
-  if(email.match(" ")||email.match(" "))
-  {
-    errormessage.innerText="Enter Proper Mail ID"
-    emailRequired.style.color="red";
-    emailid.style.border="1px solid red"
-    return false;
-  }
-  else if(!(email.match(testREgex)))
-  {
-    errormessage.innerText="Enter valid Mail ID"
-    emailRequired.style.color="red";
-    emailid.style.border="1px solid red"
-    return false;
-  }
-  errormessage.innerText=""
-  emailRequired.style.color="black";
-  emailid.style.border="1px solid black"
-  return true;
-}
-function validatePassword(){
-  let passwordid=document.getElementById("password")
-  let password=document.getElementById("password").value;
-  let errormessage=document.getElementById("passwordError");
-  let passwordRequired=document.getElementById("passwordRequired");
-  let testregex = /^[A-Za-z]\w{7,14}$/
-  // /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&-+=()])(?=\\S+$).{8, 20}$/
-  console.log(password)
-  if(!(password.match(testregex)))
-  {
-    errormessage.innerText="Enter a valid Password"
-    passwordRequired.style.color="red";
-    passwordid.style.border="red 1px solid"
-    return false;
-  }
-  errormessage.innerText=""
-  passwordRequired.style.color="black";
-  passwordid.style.border="black 1px solid"
-  return true;
-}
-function confirmpassword(){
-  let password=document.getElementById("password").value;
-  let confirmPassword=document.getElementById("confirmPassword").value;
-  if(password!=confirmPassword)
-  {
-    console.log("enter correct password")
-  }
-}
-function validateFirstName(){
-  let firstName=document.getElementById("firstName").value
-  let firstNameId=document.getElementById("firstName")
-  let errormessage=document.getElementById("firstNameError")
-  let nameRequired=document.getElementById("firstNameRequired")
-  let testregex = /^[A-Za-z]\w{7,14}$/
-  if(!(firstName.match(testregex)))
-  {
-    errormessage.innerText="Enter a valid Name"
-    nameRequired.style.color="red";
-    firstNameId.style.border="red 1px solid"
-    return false;
-  }
-  errormessage.innerText=""
-  nameRequired.style.color="black";
-  firstNameId.style.border="black 1px solid"
-  return false;
-}
+// function validateEmail(){
+//   let emailid=document.getElementById("email")
+//   let email=document.getElementById("email").value;
+//   let errormessage=document.getElementById("emailError");
+//   let emailRequired=document.getElementById("emailRequired");
+//   let testREgex='[a-z0-9]+@[a-z]+\.[a-z]{2,3}';
+//   if(email.match(" ")||email.match(" "))
+//   {
+//     errormessage.innerText="Enter Proper Mail ID"
+//     emailRequired.style.color="red";
+//     emailid.style.border="1px solid red"
+//     return false;
+//   }
+//   else if(!(email.match(testREgex)))
+//   {
+//     errormessage.innerText="Enter valid Mail ID"
+//     emailRequired.style.color="red";
+//     emailid.style.border="1px solid red"
+//     return false;
+//   }
+//   errormessage.innerText=""
+//   emailRequired.style.color="black";
+//   emailid.style.border="1px solid black"
+//   return true;
+// }
+// function validatePassword(){
+//   let passwordid=document.getElementById("password")
+//   let password=document.getElementById("password").value;
+//   let errormessage=document.getElementById("passwordError");
+//   let passwordRequired=document.getElementById("passwordRequired");
+//   let testregex = /^[A-Za-z]\w{7,14}$/
+//   // /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&-+=()])(?=\\S+$).{8, 20}$/
+//   console.log(password)
+//   if(!(password.match(testregex)))
+//   {
+//     errormessage.innerText="Enter a valid Password"
+//     passwordRequired.style.color="red";
+//     passwordid.style.border="red 1px solid"
+//     return false;
+//   }
+//   errormessage.innerText=""
+//   passwordRequired.style.color="black";
+//   passwordid.style.border="black 1px solid"
+//   return true;
+// }
+// function confirmpassword(){
+//   let password=document.getElementById("password").value;
+//   let confirmPassword=document.getElementById("confirmPassword").value;
+//   if(password!=confirmPassword)
+//   {
+//     console.log("enter correct password")
+//   }
+// }
+// function validateFirstName(){
+//   let firstName=document.getElementById("firstName").value
+//   let firstNameId=document.getElementById("firstName")
+//   let errormessage=document.getElementById("firstNameError")
+//   let nameRequired=document.getElementById("firstNameRequired")
+//   let testregex = /^[A-Za-z]\w{7,14}$/
+//   if(!(firstName.match(testregex)))
+//   {
+//     errormessage.innerText="Enter a valid Name"
+//     nameRequired.style.color="red";
+//     firstNameId.style.border="red 1px solid"
+//     return false;
+//   }
+//   errormessage.innerText=""
+//   nameRequired.style.color="black";
+//   firstNameId.style.border="black 1px solid"
+//   return false;
+// }
 function clickBasicForm(){
   // let firstName=document.getElementById("firstName").value
   // let password=document.getElementById("password").value;
@@ -180,3 +169,122 @@ function clickTeamForm(){
   formStepsNum++;
   updateFormSteps();
 }
+
+
+
+// VALIDATION
+class Validation{
+  alphaRegex=/^[a-zA-Z ]+$/;
+  numberRegex=/^[6789]{1}[\d]{9}$/;
+  emailUserRegex=/^([a-z]+[\.-\d]*)@$/;
+  emailDomainRegex=/^([a-z-]+)\.([a-z\-]{2,8})(\.[a-z]{2,8})?$/;
+  dateRegex=/^(\d{4})\-(0[1-9]|1[0-2])\-(0[1-9]|[12][0-9]|3[01])$/; 
+  user={
+    email:"",
+    firstName:"",
+    password:""
+  }
+}
+// function validateFirstName(){
+//   let firstName=document.getElementById("firstName").value
+//   console.log(firstName)
+//   // if(this.user.firstName == ""){
+//   //     this.user.firstNameError = "Firstname required";
+//   //     this.displayBasicErrorMessage(3,this.user.firstNameError);
+//   // }
+//   // else if(this.alphaRegex.test(this.user.firstName) == false){
+//   //     this.user.firstNameError = "Should contain only alphabets";
+//   //     this.displayBasicErrorMessage(3,this.user.firstNameError);
+//   // }
+//   // else if(this.user.firstName.length < 3)
+//   // {
+//   //     this.user.firstNameError = "Should contain atleast 3 letters";
+//   //     this.displayBasicErrorMessage(3,this.user.firstNameError);
+//   // }
+//   // else if(this.user.firstName.length > 15)
+//   // {
+//   //     this.user.firstnameError = "Should not exceed 15 letters";
+//   //     this.displayBasicErrorMessage(3,this.user.firstNameError);
+//   // }
+//   // else 
+//   // {
+//   //     this.removeBasicErrorMessage(3);
+//   //     this.user.firstNameError = "";
+//   // }
+// }
+let validate = new Validation();
+function validateEmail()
+{
+  let formEmail=document.getElementById("email").value;
+  validate.user.email=formEmail;
+  if(formEmail=="")
+  {
+    emailError("Email ID Required")
+  }
+  else if(validate.emailDomainRegex.test(formEmail.substring((0,formEmail.indexOf("@"))+1))==false)
+  {
+    emailError("Enter a valid mail ID")
+  }
+  else if(validate.emailDomainRegex.test(formEmail.substring((formEmail.indexOf("@"))+1,formEmail.length))==false)
+  {
+    emailError("Enter a valid Domain")
+  }
+  else{
+    emailCorrect();
+  }
+}
+
+function validatePassword()
+{
+  let formPassword=document.getElementById("password");
+  validate.user.password=formPassword;
+  if(formPassword=="")
+  {
+    passwordError("Password is Required");
+  }
+  else{
+    passwordCorrect();
+  }
+}
+
+function emailError(errorMessage)
+{
+  let emailid=document.getElementById("email")
+  let errormessage=document.getElementById("emailError");
+  let emailRequired=document.getElementById("emailRequired");
+  errormessage.innerText=errorMessage
+  emailRequired.style.color="red";
+  emailid.style.border="1px solid red"
+  return false;
+}
+function emailCorrect()
+{
+  let errormessage=document.getElementById("emailError");
+  let emailRequired=document.getElementById("emailRequired");
+  let emailid=document.getElementById("email")
+  errormessage.innerText=""
+  emailRequired.style.color="black";
+  emailid.style.border="1px solid black"
+  return true;
+}
+function passwordError(errorMessage)
+{
+  let passwordId=document.getElementById("password")
+  let passwordMessage=document.getElementById("passwordError")
+  let passwordRequired=document.getElementById("passwordRequired")
+  passwordMessage.innerText=errorMessage
+  passwordRequired.style.color="red";
+  passwordId.style.border="1px solid red"
+  return false;
+}
+function passwordCorrect()
+{
+  let passwordId=document.getElementById("password")
+  let passwordMessage=document.getElementById("passwordError")
+  let passwordRequired=document.getElementById("passwordRequired")
+  passwordMessage.innerText=""
+  passwordRequired.style.color="black";
+  passwordId.style.border="1px solid black"
+  return true;
+}
+
